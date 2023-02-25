69fa85396fab7dcebd605a1cd96b56d8f0983020
Alwin-Varghese-T <90436500+Alwin-Varghese-T@users.noreply.github.com>
2023-02-25 11:48:13 +0530

    Add files via upload

---
+++ abab/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# main page
+
+@app.route('/')
+
+@app.route('/pages-login.html')
+def login():
+    return render_template('pages-login.html')
+
+@app.route('/index.html')
+def home():
+
+    items = {'name' : 'Scheme name1', 'link' : 'https://email.gov.in/' },{'name' : 'Scheme name2', 'link' : 'https://email.gov.in/' },{'name' : 'Scheme name3' ,'link' : 'https://email.gov.in/' },{'name' : 'Scheme name4', 'link' : 'https://email.gov.in/' },{'name' : 'Scheme name5' ,'link' : 'https://email.gov.in/' }
+
+
+    return render_template('index.html', items=items)
+
+@app.route('/users-profile.html')
+def user():
+    return render_template('users-profile.html')
+
+@app.route('/pages-register.html')
+def register():
+    return render_template('pages-register.html',form=form)
+
+if __name__ == '__main__':
+    app.run()
+
+    
